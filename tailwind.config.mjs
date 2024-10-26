994d10d3f7a6eb1e16e8bc4ae4caade3fd50d3f8
isaias-alt <cascolucasisaias@gmail.com>
2024-10-25 21:54:11 -0300

    setting up prettier and eslint

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				primary: '#62060F',
-				secondary: '#DBD8C8',
-			},
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#62060F",
+        secondary: "#DBD8C8",
+      },
+    },
+  },
+  plugins: [],
+};
