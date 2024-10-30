92a1a2f00a5555c0f990d7ca282e89f6b77049ec
isaias-alt <cascolucasisaias@gmail.com>
2024-10-30 12:02:27 -0300

    add flowbite

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}", 
+    './node_modules/flowbite/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+  ],
   theme: {
     extend: {
       colors: {
@@ -11,5 +14,6 @@ export default {
   },
   plugins: [
     require("@tailwindcss/typography"),
+    require("flowbite/plugin"),
   ],
 };
