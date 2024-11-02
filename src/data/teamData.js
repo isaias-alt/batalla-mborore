ffa6472abdf420f8ef7ccd4a609b7798cdcc53c1
Carlos Larré <carloslarre.cl@gmail.com>
2024-11-02 15:26:26 -0300

    añadido las tarjetas

---
+++ src/data/teamData.js
@@ -0,0 +1,31 @@
+// teamData.js
+export const teamMembers = [
+    {
+      name: "Lucas Isaías Casco",
+      role: "Desarrollador Web - Actor de voz",
+      imageUrl: "https://via.placeholder.com/100",
+      instagramUrl: "#",
+      linkedinUrl: "#",
+      githubUrl: "#"
+    },
+    
+    {
+        name: "Arnaldo Fabian Borda Martinez",
+        role: "Desarrollador Web",
+        imageUrl: "https://via.placeholder.com/100",
+        instagramUrl: "#",
+        linkedinUrl: "#",
+        githubUrl: "#"
+    },
+    
+    {
+        name: "Carlos Adolfo Larré Romero",
+        role: "Desarrollador Web",
+        imageUrl: "https://via.placeholder.com/100",
+        instagramUrl: "https://www.instagram.com/larre_carlos02/?hl=es",
+        linkedinUrl: "https://www.linkedin.com/in/carloslarrero/",
+        githubUrl: "https://github.com/carloslarrero"
+    },
+    // agrega más miembros aquí
+  ];
+  
