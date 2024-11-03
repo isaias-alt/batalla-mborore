4f969c54e72854da76be0473a8334af0fb80bf77
Carlos Larré <carloslarre.cl@gmail.com>
2024-11-03 02:03:33 -0300

    images on the cards

--- src/data/teamData.js
+++ src/data/teamData.js
@@ -1,29 +1,248 @@
 export const TEAM_MEMBERS = [
-    {
-      name: "Lucas Isaías Casco",
-      role: "Desarrollador Web - Actor de voz",
-      imageUrl: "https://via.placeholder.com/100",
-      instagramUrl: "#",
-      linkedinUrl: "#",
-      githubUrl: "#"
-    },
-    
-    {
-        name: "Arnaldo Fabian Borda Martinez",
-        role: "Desarrollador Web",
-        imageUrl: "https://via.placeholder.com/100",
-        instagramUrl: "#",
-        linkedinUrl: "#",
-        githubUrl: "#"
-    },
-    
-    {
-        name: "Carlos Adolfo Larré Romero",
-        role: "Desarrollador Web",
-        imageUrl: "/responsables/imagen.png",
-        instagramUrl: "https://www.instagram.com/larre_carlos02/?hl=es",
-        linkedinUrl: "https://www.linkedin.com/in/carloslarrero/",
-        githubUrl: "https://github.com/carloslarrero"
-    },
-    // agrega más miembros aquí
-  ]; 
+  {
+    name: "Carmen Anahí Romero Marín ",
+    role: "Desarrollo del vídeo",
+    imageUrl: "/responsables/Anahi_Romero.webp",
+    instagramUrl:
+      "https://www.instagram.com/_anni_marin/profilecard/?igsh=MWYybXlkcjdsa2RiYQ==",
+    linkedinUrl:
+      "https://www.linkedin.com/in/anahi-romero-9524a4249?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    githubUrl: "",
+  },
+
+  {
+    name: "Stephanie Micaela Bustamante Alfonso",
+    role: "Dirección y Producción",
+    imageUrl: "/responsables/Mica_Bustamante.webp",
+    instagramUrl: "https://www.instagram.com/mica_bustamante00",
+    linkedinUrl: "",
+    githubUrl: "",
+  },
+
+  {
+    name: "Iván Daniel Cardozo Gonzalez",
+    role: "Dirección",
+    imageUrl: "/responsables/Ivan_Cardozo.webp",
+    instagramUrl: "https://www.instagram.com/idcardozo78/?hl=es",
+    linkedinUrl: "",
+    githubUrl: "",
+  },
+
+  {
+    name: "Pablo Sebastián Delvalle Velázquez",
+    role: "Narrador",
+    imageUrl: "/responsables/Pablo.webp",
+    instagramUrl:
+      "https://www.instagram.com/pablodelvalle7/profilecard/?igsh=cWM3MDFhYWtmZnhs",
+    linkedinUrl: "",
+    githubUrl: "",
+  },
+
+  {
+    name: "Carlos Adolfo Larré Romero",
+    role: "Desarrollador Web",
+    imageUrl: "/responsables/Carlos_Larre.webp",
+    instagramUrl: "https://www.instagram.com/larre_carlos02/?hl=es",
+    linkedinUrl: "https://www.linkedin.com/in/carloslarrero/",
+    githubUrl: "https://github.com/carloslarrero",
+  },
+
+  {
+    name: "Lucas Isaias Casco",
+    role: "Desarrollador Web - Actor de voz",
+    imageUrl: "/responsables/Lucas.webp",
+    instagramUrl: "https://www.instagram.com/lucascodev",
+    linkedinUrl: "https://www.linkedin.com/in/lucascodev/",
+    githubUrl: "https://github.com/isaias-alt/",
+  },
+
+  {
+    name: "Arnaldo Fabian Borda Martinez",
+    role: "Desarrollador Web",
+    imageUrl: "/responsables/avatar.webp",
+    instagramUrl: "https://www.instagram.com/lucascodev",
+    linkedinUrl: "https://www.linkedin.com/in/lucascodev/",
+    githubUrl: "https://github.com/isaias-alt/",
+  },
+
+  {
+    name: "Perla Mariana Oribe Flores",
+    role: "Asesor de Dialecto",
+    imageUrl: "/responsables/Perla.webp",
+    instagramUrl: "https://www.instagram.com/perlaoribe/profilecard/?igsh=MWV1MDB5aHQ2N2E5bQ==",
+    linkedinUrl: "",
+    githubUrl: "",
+  },
+
+  {
+    name: "Carlos Ivan Bolla Peralta",
+    role: "Actor de Voz",
+    imageUrl: "/responsables/Ivan_Bolla.webp",
+    instagramUrl: "https://www.instagram.com/bolla.ivan_77?igsh=NHoyZzI2ZmllOWZr",
+    linkedinUrl: "https://www.linkedin.com/in/ivan-bolla",
+    githubUrl: "",
+  },
+
+  {
+    name: "Mari Sol Sarabia Gomez",
+    role: "Asesor de Dialecto",
+    imageUrl: "/responsables/MariSol.webp",
+    instagramUrl: "https://www.instagram.com/marisolsarabiag?igsh=eG52eGdmNzdmbGUw&utm_source=qr",
+    linkedinUrl: "",
+    githubUrl: "",
+  },
+
+  {
+    name: "Oscar Samuel Corvalán Valenzuela",
+    role: "Localización Geográfica - Actor de Voz",
+    imageUrl: "/responsables/Samu.webp",
+    instagramUrl: "https://www.instagram.com/corvalan_samuel/profilecard/?igsh=bnRvNTJhZDlmbmE5",
+    linkedinUrl: "",
+    githubUrl: "",
+  },
+
+  {
+    name: "Fabian Rolon Pedrozo",
+    role: "Localización Geográfica",
+    imageUrl: "/responsables/FabianRP.webp",
+    instagramUrl: "https://www.instagram.com/f_r_p_k_b/profilecard/?igsh=MTd5MGlmc3ltaHVjMA==",
+    linkedinUrl: "",
+    githubUrl: "",
+  },
+
+  {
+    name: "Guillermo Argüello",
+    role: "Desarrollador Web",
+    imageUrl: "/responsables/Guillermo.webp",
+    instagramUrl: "https://www.instagram.com/memoarguellodev/profilecard/?igsh=dHZ3OXhvcHNsYWVv",
+    linkedinUrl: "https://py.linkedin.com/in/guillermo-arg%C3%BCello-cordob%C3%A9s-ba02b71b5",
+    githubUrl: "https://github.com/MemoArguello",
+  },
+
+  {
+    name: "Carlos Antonio Barboza Vázquez",
+    role: "Desarrollador Web",
+    imageUrl: "/responsables/Carlos_Barboza.webp",
+    instagramUrl: "https://www.instagram.com/carlosbarboza39/profilecard/?igsh=MTRoNG5ra24zbmcxcQ==",
+    linkedinUrl: "https://py.linkedin.com/in/carlos-barboza-274144209",
+    githubUrl: "https://github.com/Barboza13",
+  },
+
+  {
+    name: "Luis Herrera",
+    role: "Actor de Voz",
+    imageUrl: "/responsables/Luis_Herrera.webp",
+    instagramUrl: "https://www.instagram.com/luisahm14/profilecard/?igsh=aXZ0anltdnYwYXFv",
+    linkedinUrl: "https://www.linkedin.com/in/luis-alberto-herrera-martinez-3b3937204?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
+    githubUrl: "https://github.com/luisrap10%0A",
+  },
+
+  {
+    name: "Aldo Fabrizio Del Puerto Muñoz ",
+    role: "Localización Geográfica",
+    imageUrl: "/responsables/Fabricio.webp",
+    instagramUrl: "https://www.instagram.com/delpuerto_fabri/profilecard/?igsh=MTVuYzNrZGNnNHM0cA==",
+    linkedinUrl: "",
+    githubUrl: "",
+  },
+
+  {
+    name: "Cesar Joaquín Alegre Báez",
+    role: "Desarrollador Web - Actor de Voz",
+    imageUrl: "/responsables/Cesar_Alegre.webp",
+    instagramUrl: "https://www.instagram.com/cesaralegre08/",
+    linkedinUrl: "https://www.linkedin.com/in/cesar-joaqu%C3%ADn-alegre-b%C3%A1ez-4179b431b/",
+    githubUrl: "https://github.com/CesarAlegre08",
+  },
+
+  {
+    name: "Aracely Estefany Vera Quiroz",
+    role: "Asesor de Dialecto",
+    imageUrl: "/responsables/Ara_Vera.webp",
+    instagramUrl: "https://www.instagram.com/aracelyvera09/profilecard/?igsh=d283a2sxaXJpaWNu",
+    linkedinUrl: "",
+    githubUrl: "",
+  },
+
+  {
+    name: "Matias Sánchez",
+    role: "Actor de Voz",
+    imageUrl: "/responsables/Matias.webp",
+    instagramUrl: "https://www.instagram.com/edgardoshz/?hl=es",
+    linkedinUrl: "https://www.linkedin.com/in/matias-sánchez-298023209/",
+    githubUrl: "https://github.com/podotito9",
+  },
+
+  {
+    name: "Abel Antonio Amarilla Ríos",
+    role: "Desarrollador Web",
+    imageUrl: "/responsables/Abel_Amarilla.webp",
+    instagramUrl: "https://www.instagram.com/abel_amarilla9/",
+    linkedinUrl: "https://www.linkedin.com/in/abelr09/",
+    githubUrl: "https://github.com/Abelr09",
+  },
+
+  {
+    name: "Sergio Adrián Sosa Veloso",
+    role: "Localización Geográfica",
+    imageUrl: "/responsables/Sergio_Sosa.webp",
+    instagramUrl: "https://www.instagram.com/serchoa_vel/profilecard/?igsh=ZDd2N2Y5MTd1ZWh0",
+    linkedinUrl: "https://www.linkedin.com/in/sergio-as-607038300?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    githubUrl: "",
+  },
+
+  {
+    name: "Roberto Carlos Cabrera Cubilla",
+    role: "Localización Geográfica",
+    imageUrl: "/responsables/Roberto.webp",
+    instagramUrl: "https://www.instagram.com/roberto_c_c_c_/?hl=es",
+    linkedinUrl: "",
+    githubUrl: "",
+  },
+
+  {
+    name: "Milagros Gamarra Salinas",
+    role: "Traducción",
+    imageUrl: "/responsables/Milagros.webp",
+    instagramUrl: "https://www.instagram.com/milagrosgam_/profilecard/?igsh=MXV0MmxmMmlyMHAycA==",
+    linkedinUrl: "",
+    githubUrl: "",
+  },
+
+  {
+    name: "José María Villar Medina",
+    role: "Actor de Voz",
+    imageUrl: "/responsables/Jose_Villar.webp",
+    instagramUrl: "https://www.instagram.com/jose16villar/?hl=es",
+    linkedinUrl: "",
+    githubUrl: "",
+  },
+
+  {
+    name: "Rubén Darío Maidana Amarilla",
+    role: "Actor de Voz",
+    imageUrl: "/responsables/Ruben.webp",
+    instagramUrl: "https://www.instagram.com/rubenmadev?igsh=Y3MyOGtsOWlnMDYz",
+    linkedinUrl: "https://www.linkedin.com/in/rub%C3%A9n-maidana-amarilla-989063212?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    githubUrl: "",
+  },
+
+  {
+    name: "Lina Vega",
+    role: "Localización Geográfica",
+    imageUrl: "/responsables/avatar.webp",
+    instagramUrl: "https://www.instagram.com/linavega_m/?hl=es",
+    linkedinUrl: "",
+    githubUrl: "",
+  },
+
+  {
+    name: "Rocio Guadalupe Romero Medina",
+    role: "Traducción",
+    imageUrl: "/responsables/Rocio_Romero.webp",
+    instagramUrl: "https://www.instagram.com/rociorom_",
+    linkedinUrl: "https://www.linkedin.com/in/rocio-romero-3184a2249",
+    githubUrl: "",
+  },
+  // agrega más miembros aquí
+];
